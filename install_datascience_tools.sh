e5851a19cb2168a38f79dad9c65d7fd35c0cbebe
Jeremie Lequeux <jlequeux@gmail.com>
2019-12-16 14:47:11 +0000

    add datascience tools, clean tmux

---
+++ install_datascience_tools.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install Python3-pip -y
+
+mkdir -p /home/jeremie/code/
+git clone git@github.com:froglabs/projects.git /home/jeremie/code/
+
+pip3 install froglabs
+pip3 install jupyter
+pip3 install -r /home/jeremie/code/projects/toolkit/requirements.txt
