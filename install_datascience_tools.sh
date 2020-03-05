9188cd197ec2048a9beb8afbada9e99037803649
Jeremie Lequeux <jlequeux@gmail.com>
2020-03-05 17:51:34 +0000

    update

--- install_datascience_tools.sh
+++ install_datascience_tools.sh
@@ -1,10 +1,6 @@
 sudo apt-get update
-sudo apt-get install Python3-pip -y
-
-mkdir -p /home/jeremie/code/
-git clone git@github.com:froglabs/projects.git /home/jeremie/code/
+sudo apt-get install python3-pip -y
 
 pip3 install froglabs
 pip3 install jupyter
 pip3 install flake8
-pip3 install -r /home/jeremie/code/projects/toolkit/requirements.txt
