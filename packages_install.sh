f4d5d85d4d79c147e96eb625261e5c1513b8f916
Jeremie Lequeux <jlequeux@gmail.com>
2020-07-19 16:58:34 +0000

    add requirement file

--- packages_install.sh
+++ packages_install.sh
@@ -2,6 +2,4 @@ sudo apt-get update
 sudo apt-get install python3-pip -y
 sudo apt-get install awscli
 
-pip3 install froglabs
-pip3 install jupyter
-pip3 install flake8
+pip3 install -r requirements.txt
